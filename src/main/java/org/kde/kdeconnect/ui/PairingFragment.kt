bb550d9ddc9baef4fdbc037c0e47e1489521fee2
Albert Vaca Cintora <albertvaka@gmail.com>
2026-07-10 19:57:00 +0200

    Missing import

--- src/main/java/org/kde/kdeconnect/ui/PairingFragment.kt
+++ src/main/java/org/kde/kdeconnect/ui/PairingFragment.kt
@@ -9,6 +9,7 @@ import android.Manifest
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager.PERMISSION_GRANTED
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
